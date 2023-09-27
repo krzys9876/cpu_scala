2964a3aa0ab7b06490c019409094ba90b4f80797
Krzysztof Ruta <krzysztof.ruta@play.pl>
2023-09-27 20:02:56 +0200

    add dummy tests for LD operation

---
+++ src/test/scala/test/OperationTest.scala
@@ -0,0 +1,27 @@
+package org.kr.cpu
+package test
+
+import org.scalatest.GivenWhenThen
+import org.scalatest.featurespec.AnyFeatureSpec
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+
+class OperationTest extends AnyFeatureSpec with GivenWhenThen with ScalaCheckPropertyChecks:
+  Feature("NOP"):
+    Scenario("NOP as LD w/o operands"):
+      assert(1==1)
+
+  Feature("LD"):
+    Scenario("load immediate (low)"):
+      assert(1==1)
+
+    Scenario("load immediate (high)"):
+      assert(1 == 1)
+
+    Scenario("copy register"):
+      assert(1 == 1)
+
+    Scenario("read memory"):
+      assert(1 == 1)
+
+    Scenario("write memory"):
+      assert(1 == 1)
