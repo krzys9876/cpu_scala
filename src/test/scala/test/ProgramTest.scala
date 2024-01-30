85d685d0a7e652dad06fab06ded073f31de2d776
Krzysztof Ruta <krzysztof.ruta@play.pl>
2024-01-30 01:13:59 +0100

    add pending test for sample programs

---
+++ src/test/scala/test/ProgramTest.scala
@@ -0,0 +1,19 @@
+package org.kr.cpu
+package test
+
+import org.scalatest.GivenWhenThen
+import org.scalatest.featurespec.AnyFeatureSpec
+
+class ProgramTest extends AnyFeatureSpec with GivenWhenThen {
+  Feature("Sequential register operations"):
+    pending
+
+  Feature("Sequential memory operations"):
+    pending
+
+  Feature("Call and return"):
+    pending
+
+  Feature("A loop with register as counter"):
+    pending
+}
