629249c7ec4ac95e209c812c2431f131afa5d2f1
Krzysztof Ruta <krzysztof.ruta@play.pl>
2024-02-22 23:20:56 +0100

    start playing with assembler

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,5 +1,42 @@
 package org.kr.cpu
 
+import assembler.Assembler
+
+import java.nio.file.{Files, Path}
+import scala.util.{Failure, Success, Try}
+import scala.jdk.CollectionConverters.ListHasAsScala
+
 @main
 def main(args: String*): Unit =
-  println("Main CPU function doing nothing")
+  if(args.length != 1)
+    println(f"provide a file name with assembly code")
+    scala.sys.exit(1)
+
+  val filePath = Path.of(args(0))
+
+  val lines: Vector[String] = Try(Files.readAllLines(filePath)) match
+    case Success(lines) => lines.asScala.toVector
+    case Failure(e) => println(f"Error occured reading file: ${filePath}\n${e.getClass.getName} / ${e.getMessage}")
+      scala.sys.exit(1)
+
+  println(f"${lines.length} lines read from file ${filePath.getFileName} (${filePath.toAbsolutePath})")
+
+  val program = lines.mkString("\n")
+  val assembler = Assembler(program)
+
+  assembler.machineCode match
+    case Right(lines) => println(lines.mkString("\n"))
+    case Left(message) => println(f"Error occured while parsing the program:\n$message")
+      scala.sys.exit(1)
+
+  // clear screen
+  print(27.toChar)
+  print("[")
+  print("2")
+  print("J")
+
+  // go home
+  print(27.toChar)
+  print("[")
+  print("H")
+
